459bfce5e6f95e71679e9d90ade6408131db8ae6
Faiz Henri <faizhenrik86@gmail.com>
2024-10-17 13:17:01 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  // Fungsi untuk menangani input tombol
+  const handlePress = (value) => {
+    if (value === '=') {
+      try {
+        // Evaluasi ekspresi matematika
+        // setResult(eval(input).toString());
+        setResult('Miss U :) mandaa');
+      } catch (error) {
+        setResult('Error');
+      }
+    } else if (value === 'C') {
+      // Clear input
+      setInput('');
+      setResult('');
+    } else {
+      setInput(input + value);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="result-container">
+        <div className="result">{result}</div>
+        <div className="input">{input}</div>
+      </div>
+      <div className="buttons-container">
+        {['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', 'C', '=', '+'].map((item) => (
+          <button key={item} className="button" onClick={() => handlePress(item)}>
+            {item}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
